5e067e00d35fd87f4d6ece936ca5fa8bc6333d0d
Nathan Meadows <nathan@negativenull.net>
2014-01-13 13:21:40 -0700

    initial python version

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from tag import Tag
+
+tags = Tag('tag')
+content = '''
+[tag:youtube("http://youtu.be/lxNORk0vKd0",300,200)] 
+[tag:helloWorld()] 
+[tag:displayLine("25px")] 
+[tag:displaySquare("25px", 99px)] 
+'''
+print tags.processTags(content)
+
